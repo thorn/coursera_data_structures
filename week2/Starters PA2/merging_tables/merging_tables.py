f191c47b3d8f12307dafca34a3f34ae326d0d229
Arsen Shamkhalov <thornu731@gmail.com>
2016-04-24 15:33:43 +0300

    feat: slow implementation for third problem

--- week2/Starters PA2/merging_tables/merging_tables.py
+++ week2/Starters PA2/merging_tables/merging_tables.py
@@ -10,22 +10,33 @@ ans = max(lines)
 
 def getParent(table):
     # find parent and compress path
+    if table != parent[table]:
+      parent[table] = getParent(parent[table])
     return parent[table]
 
 def merge(destination, source):
     realDestination, realSource = getParent(destination), getParent(source)
 
     if realDestination == realSource:
-        return False
+      return(max(lines))
 
     # merge two components
-    # use union by rank heuristic 
+    # use union by rank heuristic
     # update ans with the new maximum table size
-    
+    if rank[realDestination] > rank[realSource]:
+      parent[realSource] = realDestination
+      lines[realDestination] += lines[realSource]
+    else:
+      parent[realDestination] = realSource
+      if rank[realDestination] == rank[realSource]:
+        rank[realSource] += 1
+      lines[realSource] += lines[realDestination]
+    return(max(lines))
+
     return True
 
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
-    merge(destination - 1, source - 1)
-    print(ans)
-    
+    print(merge(destination - 1, source - 1))
+    # print(ans)
+
