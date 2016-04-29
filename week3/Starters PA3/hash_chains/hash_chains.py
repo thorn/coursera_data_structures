9265b817d79dc871c75f419854e6a4db63116fda
Arsen Shamkhalov <thornu731@gmail.com>
2016-04-29 12:41:42 +0300

    feat: implemented solution to the second week3 problem - hash_chains

--- week3/Starters PA3/hash_chains/hash_chains.py
+++ week3/Starters PA3/hash_chains/hash_chains.py
@@ -16,8 +16,7 @@ class QueryProcessor:
 
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
-        # store all strings in one list
-        self.elems = []
+        self.elems = [[] for x in range(bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -36,22 +35,22 @@ class QueryProcessor:
 
     def process_query(self, query):
         if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(reversed(self.elems[query.ind]))
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+            key_index = self._hash_func(query.s)
+            key_list = self.elems[key_index]
+            object_in_list = query.s in key_list
+
+            if query.type == "find":
+                self.write_search_result(object_in_list)
+            elif query.type == "add":
+                if object_in_list: return
+                key_list.append(query.s)
+            elif query.type == "del":
+                if not object_in_list: return
+                key_list.remove(query.s)
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                print("unsupported operation type")
 
     def process_queries(self):
         n = int(input())
